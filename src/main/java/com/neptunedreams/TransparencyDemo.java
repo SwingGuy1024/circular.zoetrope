a9b46ca9b75335a6f75e5d19285349cc509146c8
SwingGuy1024 <SwingGuy1024@yahoo.com>
2019-10-30 11:42:14 -0700

    Name changes

--- src/main/java/com/neptunedreams/TransparencyDemo.java
+++ src/main/java/com/neptunedreams/TransparencyDemo.java
@@ -24,7 +24,7 @@ import static java.awt.AlphaComposite.*;
  *
  * @author Miguel Mu\u00f1oz
  */
-public final class TransparencyTest extends JPanel {
+public final class TransparencyDemo extends JPanel {
 
   private static final int diameter = 60;
 
@@ -33,12 +33,12 @@ public final class TransparencyTest extends JPanel {
     JFrame frame = new JFrame("Blue Source with Red Destination");
     frame.setLocationByPlatform(true);
     frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    frame.add(new TransparencyTest());
+    frame.add(new TransparencyDemo());
     frame.pack();
     frame.setVisible(true);
   }
   
-  private TransparencyTest() {
+  private TransparencyDemo() {
     super(new GridLayout(0, 5, 6, 6));
     setBackground(Color.lightGray);
     for (Alpha alpha : Alpha.values()) {
@@ -51,8 +51,8 @@ public final class TransparencyTest extends JPanel {
     int d2 = diameter / 2;
     final int d4 = diameter / 4;
 
-    BufferedImage redSquare = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
-    Graphics2D g = redSquare.createGraphics();
+    BufferedImage blueCircle = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g = blueCircle.createGraphics();
     g.setComposite(Src);
     g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
@@ -62,18 +62,18 @@ public final class TransparencyTest extends JPanel {
 
     g.dispose();
 
-    BufferedImage blueCircle = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
-    Graphics2D gP = blueCircle.createGraphics();
+    BufferedImage redSquare = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D gP = redSquare.createGraphics();
     gP.setColor(Color.red);
     int delta = 4;
     int y = diameter/2;
     int size = y - delta;
-    gP.fillRect(delta, y, size, size);
+    gP.fillRect(delta, y, size, size); // Draw red square
     
-
-//    gP.setColor(Color.RED);
     gP.setComposite(alpha.composite);
-    gP.drawImage(redSquare, 0, 0, null);
+    gP.drawImage(blueCircle, 0, 0, null);
+
+    // draw border
     gP.setColor(Color.black);
     gP.setComposite(AlphaComposite.SrcOver);
     gP.drawRect(1, 1, diameter - 3, diameter - 3);
@@ -82,7 +82,7 @@ public final class TransparencyTest extends JPanel {
     
     JPanel panel = new JPanel(new BorderLayout());
     panel.setOpaque(false);
-    panel.add(new JLabel(new ImageIcon(blueCircle), JLabel.CENTER), BorderLayout.CENTER);
+    panel.add(new JLabel(new ImageIcon(redSquare), JLabel.CENTER), BorderLayout.CENTER);
     panel.add(new JLabel(alpha.name, JLabel.CENTER), BorderLayout.PAGE_END);
 
     return panel;
